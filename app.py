2db1274192f53ee2129348e31642a01cbe5fdf87
James Adair <dev@jvadair.com>
2025-05-22 20:33:49 -0400

    Create basic placeholder site

--- app.py
+++ app.py
@@ -1,3 +1,21 @@
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify, session
+from pyntree import Node
+
+app = Flask(__name__)
+config = Node("config.yml")
 
 
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route('/fonts')
+def get_font_url():
+    formatted_font_list = '&family='.join(config.fonts())
+    target = f"https://fonts.googleapis.com/css2?family={formatted_font_list}&display=swap"
+    return redirect(target)
+
+
+if __name__ == "__main__":
+    app.run()
