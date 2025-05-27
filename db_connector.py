40c780728273fff9ccca95de3589a779e0d6fccc
James Adair <dev@jvadair.com>
2025-05-27 12:20:39 -0400

    Ensure database connection timeout is properly handled adn update css for the header to look better

--- db_connector.py
+++ db_connector.py
@@ -10,7 +10,9 @@ while True:
     try:
         engine = db.create_engine(url="mysql+pymysql://{0}:{1}@{2}:{3}/{4}".format(  # me:password@host:port/db
                 dbsettings.user(), dbsettings.password(), dbsettings.host(), dbsettings.port(), dbsettings.database()
-            ))
+            ),
+            pool_pre_ping=True
+        )
         metadata = db.MetaData()
         conn = engine.connect()
         print("Database connection established!")
